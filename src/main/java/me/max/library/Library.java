860656f14249c9edee74e135313286dde227de4f
MaxiMiniJaniJos <maxberkelmans@live.nl>
2018-02-05 21:57:12 +0100

    Added basic stuff

--- src/main/java/me/max/library/Library.java
+++ src/main/java/me/max/library/Library.java
@@ -1,5 +1,6 @@
 package me.max.library;
 
+import me.max.library.listeners.PlayerInteractListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Library extends JavaPlugin {
@@ -7,11 +8,25 @@ public final class Library extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        info("Initialising listeners..");
+        try {
+            new PlayerInteractListener(this);
+        } catch (Exception e){
+            error("Error initialising listeners");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void info(String s){
+        getLogger().info(s);
+    }
+
+    public void error(String s){
+        getLogger().severe(s);
     }
 }
